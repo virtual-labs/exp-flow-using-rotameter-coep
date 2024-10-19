c23eaac57567d54f49cd211e19fa4bd02f847155
vleadadmin <admin@vlabs.ac.in>
2024-10-19 12:09:00 +0000

    https://virtual-labs.github.io/virtual-labs/exp-flow-using-rotameter-coep click on the link to test your code.

---
+++ aim.md
@@ -0,0 +1,7 @@
+### Aim 
+Measurement of flow using a Rotameter
+
+### Objectives
+- Study technique for flow measurment
+- Characterize the rotameter 
+- Identify the faults in rotameter under consideration
---
+++ simulation/src/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "Which among the following is the formula for volumetric flow rate?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Q=V/A"
+					}, {
+						"ANSID": false,
+						"content": "Q=A+V"
+					}, {
+						"ANSID": false,
+						"content": "Q=A-V"
+					},{
+						"ANSID": true,
+						"content": "Q=AV "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": " The operation of a rotameter is based on:",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Bernoulli's principle"
+					}, {
+						"ANSID": false,
+						"content": "Pascal's law "
+					}, {
+						"ANSID": true,
+						"content": "Gravity and fluid velocity "
+					}, {
+						"ANSID": false,
+						"content": "Archimedes' principle"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "The float in a rotameter rises when: ",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Fluid velocity increases"
+					}, {
+						"ANSID": false,
+						"content": "Fluid density increases"
+					}, {
+						"ANSID": false,
+						"content": "The system pressure decreases"
+					}, {
+						"ANSID": false,
+						"content": "The temperature of the fluid increases"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "The rate at which fliud flows through a closed pipe can be determined by:",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Determine the mass flow rate or volume flow rate "
+					}, {
+						"ANSID": false,
+						"content": "Determine the mass flow rate"
+					}, {
+						"ANSID": false,
+						"content": "Determine the volume flow rate"
+					}, {
+						"ANSID": false,
+						"content": "None of these"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Compressible flow is a flow that deals with ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Fluid temperature"
+					}, {
+						"ANSID": false,
+						"content": "Fluid geometry"
+					}, {
+						"ANSID": true,
+						"content": "Fluid density"
+					}, {
+						"ANSID": false,
+						"content": "Fluid Pressure"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "With the increase in pressure, the exit velocity ________",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Same"
+					}, {
+						"ANSID": true,
+						"content": "Decreases"
+					}, {
+						"ANSID": false,
+						"content": "Increases"
+					}, {
+						"ANSID": false,
+						"content": "Independent"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
